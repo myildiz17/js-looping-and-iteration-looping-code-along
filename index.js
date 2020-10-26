8cc89513290d68b55e66ede002cd0545ac355bba
myildiz17 <muhammetyildiz@gmail.com>
2020-10-25 20:50:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+// const arr=['ali','veli']
+// const event='bthd'
+function writeCards(arr,event){
+    const arr2=[]
+    for (let i=0; i<arr.length; i++){
+        arr2.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+    };
+    return arr2;
+};
+
+function countDown(num){
+    let number=num
+    while (number>-1){
+        console.log(number);
+        number--
+    }
+}
